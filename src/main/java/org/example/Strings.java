a55012f568b1601b5d7a50c19bcb0d78d3efdcd9
Tom Munyiri <tmmunyiri@gmail.com>
2023-04-11 14:09:54 +1100

    strings substring method

---
+++ src/main/java/org/example/Strings.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Strings {
+    public static void main(String[] args) {
+        String accountNo = "6777557691";
+        System.out.println("Sub string is: " + accountNo.substring(3));
+    }
+}
