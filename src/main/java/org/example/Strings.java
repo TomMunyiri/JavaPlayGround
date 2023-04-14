d1635f92487d028dd071b1849b64f08ddd0b1ec5
Tom Munyiri <tmmunyiri@gmail.com>
2023-04-14 15:57:54 +1100

    getOrDefault

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -1,8 +1,14 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Strings {
     public static void main(String[] args) {
-        String accountNo = "6777557691";
-        System.out.println("Sub string is: " + accountNo.substring(3));
+        /*String accountNo = "6777557691";
+        System.out.println("Sub string is: " + accountNo.substring(3));*/
+        HashMap<String, String> requesJson = new HashMap<>();
+        //requesJson.put("idnumber","3267327823");
+        String idNumber = requesJson.getOrDefault("idnumber", "").trim();
+        System.out.println("ID Number is: "+idNumber);
     }
 }
