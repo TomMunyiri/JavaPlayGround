1305484ec7a61027e62ee282985ad7da954f6daa
Tom Munyiri <tmmunyiri@gmail.com>
2023-05-11 09:28:24 +1100

    find a string in json obj

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 
 public class Strings {
@@ -9,6 +12,20 @@ public class Strings {
         HashMap<String, String> requesJson = new HashMap<>();
         //requesJson.put("idnumber","3267327823");
         String idNumber = requesJson.getOrDefault("idnumber", "").trim();
-        System.out.println("ID Number is: "+idNumber);
+        //System.out.println("ID Number is: "+idNumber);
+        containsString();
+    }
+
+    private static void containsString() {
+        String jsonString = "{\"message\":\"data set for messaging\",\"status\":\"200\",\"data\":\"[{\\\"RESP_DESC\\\":\\\"Contact(s) added successfully\\\",\\\"RESP_CODE\\\":\\\"00\\\"}]\"}";
+        JSONObject jsonObject = new JSONObject(jsonString);
+        String dataObj = String.valueOf(jsonObject.getString("data"));
+        System.out.println("jsonOBJ: " + dataObj);
+        JSONArray data_Array = new JSONArray(dataObj);
+        JSONObject data = data_Array.getJSONObject(0);
+        String respCode = data.getString("RESP_CODE");
+        String respDesc = data.has("RESP_DESC") ? data.getString("RESP_DESC") : "No Desc";
+        System.out.println("Resp Code: " + respCode);
+        System.out.println("Resp Desc: " + respDesc);
     }
 }
