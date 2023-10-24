02322310e51f4c531c1dfa07d09c923e43895a6a
Thomas Munyiri <munyiri.thomas@eclectics.io>
2023-10-24 10:26:30 +0300

    updates

--- src/main/java/org/example/Strings.java
+++ src/main/java/org/example/Strings.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Arrays;
@@ -17,14 +18,68 @@ public class Strings {
         //System.out.println("ID Number is: "+idNumber);
         //containsString();
         //removeBeforeAfter();
-        String agentId=null;
-        if(agentId.isEmpty()){
+        String agentId = null;
+        /*if(agentId.isEmpty()){
             System.out.println("is empty");
         }else{
             System.out.println("is not empty");
-        }
+        }*/
+        splitField54();
         System.out.println(contains("MIRINDA 300ML"));
         System.out.println(measureUnit("TRAD-MF-01", ""));
+        removeS();
+        logRequest("agentId=900424&charges=false&installedVersion=v1.5.4&jwtToken=&password=0000&requestDest=ABC&requestOrigin=MPOS&requestTime=2023-10-20 15:37:37&setLanguage=ENGLISH&deviceId=20D86C94CBF2B146&latitude=0.321493&longitude=32.5773446&osVersion=31");
+    }
+
+    private static void splitField54(){
+        String field54 = "Fredson Fenua|11001085";
+        String[] parts = field54.split("\\|");
+
+        if (parts.length > 0) {
+            String name = parts[0];
+            System.out.println(name); // This will print "Fredson Fenua"
+        } else {
+            // Handle the case where there is no '|' character in the string
+            System.out.println("Customer");
+        }
+    }
+    private static void removeS() {
+        String originalString = "6777557691";
+
+// Check if the string has at least 3 characters
+        if (originalString.length() >= 3) {
+            String modifiedString = originalString.substring(0, originalString.length() - 3);
+            System.out.println("Modified string: " + modifiedString);
+        } else {
+            System.out.println("The string is too short to remove 3 characters.");
+        }
+    }
+
+    private static void logRequest(String params) {
+        try {
+            // Split the query string into key-value pairs
+            String[] pairs = params.split("&");
+
+            // Create a JSONObject to store the key-value pairs
+            JSONObject jsonObject = new JSONObject();
+
+            for (String pair : pairs) {
+                String[] keyValue = pair.split("=");
+                if (keyValue.length == 2) {
+                    try {
+                        // Add the key-value pair to the JSONObject
+                        jsonObject.put(keyValue[0], keyValue[1]);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            // Print the `JSONObject` object to the console.
+            System.out.println(jsonObject.toString());
+        } catch (JSONException e) {
+            //Log.d(TAG, "logRequest: " + e.getLocalizedMessage());
+        }
     }
 
     private static String contains(String productCode) {
